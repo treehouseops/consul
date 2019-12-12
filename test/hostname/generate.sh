38044811211f38e3d9403043e79dd1e72fe019ac
R.B. Boyer <rb@hashicorp.com>
2019-12-12 15:05:00 -0600

    Making sure a wan "star join" involving mesh gateways allows for two secondaries to communicate.

--- test/hostname/generate.sh
+++ test/hostname/generate.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 
+# server.dc1.consul
 if [[ ! -f Alice.crt ]] || [[ ! -f Alice.key ]]; then
     echo "Regenerating Alice.{crt,key}..."
     rm -f Alice.crt Alice.key
@@ -11,6 +12,7 @@ if [[ ! -f Alice.crt ]] || [[ ! -f Alice.key ]]; then
     rm -f Alice.csr
 fi
 
+# bob.server.dc1.consul
 if [[ ! -f Bob.crt ]] || [[ ! -f Bob.key ]]; then
     echo "Regenerating Bob.{crt,key}..."
     rm -f Bob.crt Bob.key
@@ -19,10 +21,20 @@ if [[ ! -f Bob.crt ]] || [[ ! -f Bob.key ]]; then
     rm -f Bob.csr
 fi
 
+# betty.server.dc2.consul
 if [[ ! -f Betty.crt ]] || [[ ! -f Betty.key ]]; then
     echo "Regenerating Betty.{crt,key}..."
     rm -f Betty.crt Betty.key
     openssl req -new -sha256 -nodes -out Betty.csr -newkey rsa:2048 -keyout Betty.key -config Betty.cfg
     openssl ca -batch -config myca.conf -extfile Betty.ext -notext -in Betty.csr -out Betty.crt
     rm -f Betty.csr
+fi
+
+# bonnie.server.dc3.consul
+if [[ ! -f Bonnie.crt ]] || [[ ! -f Bonnie.key ]]; then
+    echo "Regenerating Bonnie.{crt,key}..."
+    rm -f Bonnie.crt Bonnie.key
+    openssl req -new -sha256 -nodes -out Bonnie.csr -newkey rsa:2048 -keyout Bonnie.key -config Bonnie.cfg
+    openssl ca -batch -config myca.conf -extfile Bonnie.ext -notext -in Bonnie.csr -out Bonnie.crt
+    rm -f Bonnie.csr
 fi
