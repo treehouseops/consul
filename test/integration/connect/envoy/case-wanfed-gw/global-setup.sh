01bc02121c7b6f1d8f30e193db25991f445dac17
R.B. Boyer <rb@hashicorp.com>
2020-01-17 11:25:17 -0600

    don't run the consul binary on the host

--- test/integration/connect/envoy/case-wanfed-gw/global-setup.sh
+++ test/integration/connect/envoy/case-wanfed-gw/global-setup.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+set -euo pipefail
+
 TLS_DIR="workdir/tls/${CASE_DIR}"
 
 rm -rf "${TLS_DIR}"
 mkdir -p "${TLS_DIR}"
 
-(
-set -euo pipefail
-cd "${TLS_DIR}"
-consul tls ca create
-consul tls cert create -dc=primary -server -additional-dnsname='pri.server.primary.consul'
+readonly container="consul-envoy-integ-tls-init--${CASE_DIR}"
+
+readonly scriptlet="
+mkdir /out ;
+cd /out ;
+consul tls ca create ;
+consul tls cert create -dc=primary -server -additional-dnsname='pri.server.primary.consul' ;
 consul tls cert create -dc=secondary -server -additional-dnsname='sec.server.secondary.consul'
+"
+
+docker rm -f "$container" &>/dev/null || true
+docker run -i --net=none --name="$container" consul-dev:latest sh -c "${scriptlet}"
+output_files=(
+    consul-agent-ca-key.pem
+    consul-agent-ca.pem
+    primary-server-consul-0-key.pem
+    primary-server-consul-0.pem
+    secondary-server-consul-0-key.pem
+    secondary-server-consul-0.pem
 )
+for f in "${output_files[@]}"; do
+    docker cp "${container}:/out/$f" "${TLS_DIR}"
+done
+docker rm -f "$container" >/dev/null
